0fca67becc77d5e0d5de93f945a35845547f4e6b
katherinachung <yizhong@cca.edu>
2018-02-06 17:57:40 -0800

    new

--- exercise/jquery-events/assets/script.js
+++ exercise/jquery-events/assets/script.js
@@ -1,23 +1,59 @@
-
-
-
 $(document).ready(function(){
-var nouns = ["a", "ghost", "donkey", "b", "apple", "pumpkin", "buttom", "cake", "broing", "yes", "no", "lamp", "jump", "I", "break", "sleep", "ask", "boat", "rabbit"];
-var word;
-var adjective = ["s", "mad", "moment", "loud", "bubble", "treasure", "down","chuck", "nerdy", "bread", "crash", "dotor", "brain", "atomc", "rust", "cry", "food", "laugh", "witness", "drunk"]
-function random(list) {
-  word = Math.floor((Math.random() * list.length) + 0);
-  word = list[word];
-  console.log(word);
-}
-$("#button").click(function(){
-random(adjective);
-  var x = document.getElementById("button1").innerHTML=word;
-  random(nouns);
-  var x = document.getElementById("button2").innerHTML=word;
-  random(nouns);
-  var x = document.getElementById("button3").innerHTML=word;
-  var x = document.getElementById("button4").innerHTML="";
-})
 
-})
+	var scrollPos;
+	var xPos;
+	var yPos;
+	var winWidth;
+	var winHeight;
+
+	// check when the user scrolls
+	$(window).scroll(function(){
+
+		// update the scroll position variable
+		scrollPos = $('body').scrollTop();
+
+		// update the web page with the variables value
+		$('#scroll-pos').html("scroll: " + scrollPos);
+
+	});
+
+
+	// check when the mouse moves
+	$(document).mousemove(function(e){
+
+		// update mouse x and y position
+		xPos = e.pageX;
+		yPos = e.pageY;
+		$('#mouse-pos').html('x-pos: ' + xPos + '<br />y-pos: ' + yPos);
+
+		// change div with the id "element"s CSS when you move your mouse
+		$('#element').css({
+			'transform' : 'rotate3d(' + xPos/100 + ',130,100,' + xPos + 'deg) scale(' + xPos/200 + ')',
+			'background-color' : 'rgb('+ yPos/5 + ', ' + xPos/3 + ', ' + (xPos*yPos)	/10 + ')',
+		});
+	});
+
+
+	// check when the user clicks
+	$(document).click(function(e){
+
+		// store the click position
+		xPos = e.pageX;
+		yPos = e.pageY;
+
+		// add text to the page where the user clicks
+		$('.container').append('<div style="top: ' + yPos +'px; left:' + xPos + 'px; position: absolute">' + xPos + ', ' + yPos + '</div>')
+
+	});
+
+	// check when the window is resized
+	$(window).resize(function() {
+
+		// update the webpage with the window dimensions
+		winWidth = $(window).width();
+		winHeight = $(window).height();
+		$('#window-size').html('window width: ' + winWidth + '<br />window height: ' + winHeight);
+	});
+
+
+});
